b8f6c6962da958c863b68ecd2ad0fef1cbdf85fe
wooje <woojebiz@gmail.com>
2021-03-29 08:26:26 +0900

    middleware

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func setupLogOutPut() {
 	// go 는 date 형식이 특이하다
 	// 2006-01-02 15:04:05
 	logFileName := "gin-web" + "_" + time.Now().Format("2006-01-02_150405") + ".log"
-	logPath := "./storage/log"
+	logPath := "./storage/log/"
 	f, _ := os.Create(logPath + logFileName)
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 }
