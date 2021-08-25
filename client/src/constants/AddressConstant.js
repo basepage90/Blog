dc7371a88511ca711fe6c17b8e20d06bc5107ef1
wooje <woojebiz@gmail.com>
2021-08-26 03:12:51 +0900

    edit publisher

--- client/src/constants/AddressConstant.js
+++ client/src/constants/AddressConstant.js
@@ -1,4 +1,4 @@
-// export const BaseURL = "http://crispyblog.kr"
-export const BaseURL = "http://wjk.ddns.net"
+export const BaseURL = "http://crispyblog.kr"
+// export const BaseURL = "http://wjk.ddns.net"
 export const ClientPort = ":80";
 export const ServerPort = ":5000";
