59db211146f4a0e58310fb4978d742d92367bca6
wooje <woojebiz@gmail.com>
2021-09-05 02:18:56 +0900

    https

--- client/src/constants/AddressConstant.js
+++ client/src/constants/AddressConstant.js
@@ -1,4 +1,4 @@
-export const BaseURL = "https://crispyblog.kr"
+export const BaseURL = "http://crispyblog.kr"
 // export const BaseURL = "http://wjk.ddns.net"
 export const ClientPort = ":443";
 export const ServerPort = ":5000";
