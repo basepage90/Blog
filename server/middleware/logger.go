13006177225f9e640ce028dbe86ef9918c96ae35
basepage90 <woojebiz@gmail.com>
2023-09-20 11:24:30 +0900

    fix logger

--- server/middleware/logger.go
+++ server/middleware/logger.go
@@ -11,11 +11,11 @@ func Logger() gin.HandlerFunc {
 	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
 		layout := "2006-01-02 15:04:05.000"
 		loc, _ := time.LoadLocation("Asia/Seoul")
-		kstTime := param.TimeStamp.In(loc).Format(layout)
+		kst := param.TimeStamp.In(loc).Format(layout)
 
 		return fmt.Sprintf("%s - [%s] %s %s %d %s \n",
 			param.ClientIP,
-			kstTime,
+			kst,
 			param.Method,
 			param.Path,
 			param.StatusCode,
