e91752f5f20ccf972b9be16d3dbefedacf5549a2
wjkim <wjkim@yanadoocorp.com>
2024-03-12 14:38:54 +0900

    페이징 추가

--- client/src/containers/card/CardListViewer.tsx
+++ client/src/containers/card/CardListViewer.tsx
@@ -6,14 +6,39 @@ import { GetCardList } from 'gql/query'
 import { useSelector } from 'react-redux'
 import Meta from 'components/common/helmet/Meta'
 import { RootState } from "store/store";
+import { useEffect } from 'react'
+import { useInView } from "react-intersection-observer"
+
+
+let cursorId = 0;
+const limit = 10;
 
 const CardListViewer = () => {
     const { categoryLg, categoryMd } = useParams<{categoryLg: string, categoryMd: string}>();
-    const { loading, data } = useQuery(GetCardList, {
-        variables: { category_lg : String(categoryLg), category_md: String(categoryMd) }
-    });
 
-    const { admin_flag } = useSelector( (state: RootState) => ({admin_flag: state.user.admin_flag}) );
+     const { loading, data, fetchMore } = useQuery(GetCardList,{
+         variables: { cursorId: 0, category_lg: String(categoryLg), category_md: String(categoryMd, limit: limit },
+         fetchPolicy: "cache-first"
+    })
+
+    const { admin_flag } = useSelector((state: RootState) => ({ admin_flag: state.user.admin_flag }));
+    
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if(inView === true){
+            // offset = offset + limit;
+            fetchMore({
+                variables: { cursorId: cursorId, limit: limit },
+                updateQuery: (prev, { fetchMoreResult }) => {
+                    if (!fetchMoreResult) return prev;
+                    return Object.assign({}, prev, {
+                        articlesList: [...prev.articlesList, ...fetchMoreResult.articlesList]
+                    });
+                }
+            })
+        }
+    },[inView,fetchMore]);
 
     if(loading || categoryLg === undefined){
         return(
@@ -28,6 +53,7 @@ const CardListViewer = () => {
                         admin_flag === false && articlesData.privacy === "private" ? null : <Card data={articlesData} key={key}/>
                     )}
                 </GridContainer>
+                <div ref={ref} style={{visibility:'hidden', position: 'absolute', bottom: '300px', width:'1px',height:'1px'}} />
             </>
         )
     }
